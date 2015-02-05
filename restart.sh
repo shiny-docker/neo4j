44e2825c5b2d872c34a75289a239a2015de754b9
Reese Wilson <reese@shinymayhem.com>
2015-02-05 05:27:50 +0000

    exposing port in dockerfile

--- restart.sh
+++ restart.sh
@@ -2,4 +2,4 @@
 docker stop neo4j
 docker rm neo4j
 #start the neo4j server in the foreground
-docker run -d -p 7474 --volumes-from neo4j-data --name neo4j shinymayhem/neo4j
+docker run -d --volumes-from neo4j-data --name neo4j shinymayhem/neo4j
